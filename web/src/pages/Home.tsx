bdf63d1f20aa48917f2e6bec51b52f6399fccf25
Toby Cm <toby@vsus.app>
2024-09-15 21:28:12 -0700

    Welcome to Shorl, !

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
 
   return (
     <Flex w="100%" h="100%" direction="column" justify="center" align="center">
-      <Title order={2}>Welcome to Shorl, {user.name ?? user.username ?? user.email}!</Title>
+      <Title order={2}>Welcome to Shorl, {user.name || user.username || user.email}!</Title>
 
       {user.permissions.includes("create") && <CreateButton />}
 
